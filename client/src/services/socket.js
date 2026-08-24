a96f1eb940a63b9abc919fab973a32df7ee1bf12
brainodevo-cpu <brainodevo@gmail.com>
2026-08-25 00:26:37 +0530

    Connect frontend to deployed backend

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io(
+    import.meta.env.VITE_SOCKET_URL ||
+    "http://localhost:5000"
+);
 
 export default socket;
